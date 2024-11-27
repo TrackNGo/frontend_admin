1b4ebd3b771536cea1e31c5055f76677eeae889f
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-11-28 00:23:19 +0530

    refactor: test_page_change

--- src/pages/testing/Testing.tsx
+++ src/pages/testing/Testing.tsx
@@ -1,20 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import Login from "../auth/Login"
 import ChangePassword from "../auth/ChangePassword"
 import CreateAccount from "../auth/CreateAccount"
 import AddBus from "../addBus/AddBus"
 import AddRoutes from "../addRoutes/AddRoutes"
+import Dashboard from "../dashboard/Dashboard"
 
 const Testing = () => {
   return (
     <div>
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <ul>
+          <Link to="/"><li>dashboard</li></Link>
+          <Link to="/login"><li>login</li></Link>
+          <Link to="/changepassword"><li>changepassword</li></Link>
+          <Link to="/createaccount"><li>createaccount</li></Link>
+          <Link to="/addbus"><li>addbus</li></Link>
+          <Link to="/addbusroute"><li>addbusroute</li></Link>
+        </ul>
+        <hr />
+        <br />
+        <hr />
+        <br />
+        <hr />
         <Routes>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/changepassword' element={<ChangePassword />}/>
-          <Route path='/createaccount' element={<CreateAccount />}/>
-          <Route path='/addbus' element={<AddBus />}/>
-          <Route path='/addbusroute' element={<AddRoutes />}/>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/changepassword' element={<ChangePassword />} />
+          <Route path='/createaccount' element={<CreateAccount />} />
+          <Route path='/addbus' element={<AddBus />} />
+          <Route path='/addbusroute' element={<AddRoutes />} />
         </Routes>
       </BrowserRouter>
     </div>
