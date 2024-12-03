315b976701159c6a55849975ee8c88e56e52a60c
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-04 00:12:45 +0530

    feat: nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import NavBar from './components/navbar/NavBar'
 import Testing from './pages/testing/Testing'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <div>
+        <NavBar/>
         <h1 className='text-3xl font-bold'>admin dashboard</h1>
         <Testing/>
       </div>
