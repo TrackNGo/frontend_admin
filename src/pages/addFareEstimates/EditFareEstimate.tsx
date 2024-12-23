d8f681ccc9a760a985388a07b4bd7ece7501a9a8
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-24 02:48:51 +0530

    feat: add Headline component to EditFareEstimate for improved UI

--- src/pages/addFareEstimates/EditFareEstimate.tsx
+++ src/pages/addFareEstimates/EditFareEstimate.tsx
@@ -1,9 +1,11 @@
+import Headline from "../../components/headline/Headline"
+
 const EditFareEstimate = () => {
-  return (
-    <div>
-      EditFareEstimate
-    </div>
-  )
+    return (
+        <div className='px-2'>
+            <Headline title="Edit Fare Estimate" />
+        </div>
+    )
 }
 
 export default EditFareEstimate
