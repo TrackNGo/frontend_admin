981948f4970b7135e6a96fdaaf5375f4abe9101e
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-25 01:57:48 +0530

    feat: add additional DashBoardCard components for active and non-active buses

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -8,9 +8,24 @@ const Dashboard = () => {
     count: 150,
     description: 'Total Number of the Buses Estimate in the System.',
     iconClass: 'fas fa-bus',
-    iconColor: 'text-blue-500',
+    iconColor: 'text-blue-500 hover:text-blue-600',
   }
-
+  const cards1 =
+  {
+    title: 'Active Buses',
+    count: 100,
+    description: 'Total Number of the Buses Estimate in the System.',
+    iconClass: 'fas fa-bus',
+    iconColor: 'text-green-500 hover:text-green-600',
+  }
+  const cards2 =
+  {
+    title: 'Non-Active Buses',
+    count: 100,
+    description: 'Total Number of the Buses Estimate in the System.',
+    iconClass: 'fas fa-bus',
+    iconColor: 'text-red-500 hover:text-red-600',
+  }
 
   return (
     <div className="px-2">
@@ -24,6 +39,20 @@ const Dashboard = () => {
               count={cards.count}
               iconClass={cards.iconClass}
               iconColor={cards.iconColor}
+            />
+            <DashBoardCard
+              title={cards1.title}
+              description={cards1.description}
+              count={cards1.count}
+              iconClass={cards1.iconClass}
+              iconColor={cards1.iconColor}
+            />
+            <DashBoardCard
+              title={cards2.title}
+              description={cards2.description}
+              count={cards2.count}
+              iconClass={cards2.iconClass}
+              iconColor={cards2.iconColor}
             />
           </div>
         </div>
