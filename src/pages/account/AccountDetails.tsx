cf1d8b6af7fd4319152b2f9f6d00b146d7cc12f1
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-15 23:33:12 +0530

    feat: add Back button to AccountDetails for improved navigation

--- src/pages/account/AccountDetails.tsx
+++ src/pages/account/AccountDetails.tsx
@@ -149,13 +149,19 @@ const AccountDetails = () => {
                         >
                             Edit
                         </button>
-
                         <button
                             onClick={handleDelete}
                             disabled={!userDetails}
                             className="bg-red-600 text-white px-4 mt-5 py-2 rounded-lg shadow-md transform transition-all duration-300 ease-in-out hover:bg-red-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-500"
                         >
                             Delete
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => navigate(-1)}
+                            className="px-4 mt-5 py-2 bg-gray-400 hover:bg-gray-500 text-white rounded-md transition duration-200"
+                        >
+                            Back
                         </button>
                     </div>
                 )}
